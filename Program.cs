4f8c4b3008fdac1c96c8c4ade13ee60dfa1d565d
Umut Özel <umutozel@gmail.com>
2019-10-16 14:26:50 +0300

    create files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace advanced_csharp
 {
@@ -7,6 +9,17 @@ namespace advanced_csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var path = @"C:\Code\github.com\DogusTeknoloji\advanced-csharp";
+            var directories = Directory.GetDirectories(path)
+                .Select(d => Path.GetFileName(d))
+                .Where(d => char.IsNumber(d[0]))
+                .ToList();
+                
+            foreach (var dir in directories) {
+                var filePath = Path.Combine(path, dir);
+                File.Create(filePath);
+            }
         }
     }
 }
