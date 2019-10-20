e24c3ff7497a17ded861dccbd53276f1f7acf819
Umut Özel <umutozel@gmail.com>
2019-10-21 00:03:31 +0300

    add async stream

--- 8/8.4-AsyncStreams/AsyncStreams.cs
+++ 8/8.4-AsyncStreams/AsyncStreams.cs
@@ -1,6 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Console;
+
 namespace AdvancedCSharp {
 
     public class AsyncStreams : ITutorial {
-        
+
+        public async void Run() {
+            await foreach (var dataPoint in Helper.FetchIOTData()) {
+                WriteLine(dataPoint);
+            }
+        }
+
+        public static class Helper {
+
+            public static async IAsyncEnumerable<int> FetchIOTData() {
+                for (int i = 1; i <= new Random().Next(10, 100); i++) {
+                    await Task.Delay(333);
+                    yield return i;
+                }
+            }
+        }
     }
 }
